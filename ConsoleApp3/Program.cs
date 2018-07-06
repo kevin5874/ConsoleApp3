b36ddc0529d2a336c1ab8a4d79e597cb469e0a79
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-07-05 22:21:31 -0500

    tech academy assignment

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,34 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x = Double.Parse(Console.ReadLine());
+            x *= 50;
+            printConsole(x);
+            x = getInput();
+            x += 25;
+            printConsole(x);
+            x = getInput();
+            x /= 12.5;
+            printConsole(x);
+            x = getInput();
+            bool y = x > 50 ? true : false;
+            Console.WriteLine(y);
+            x = getInput();
+            x %= 7;
+            printConsole(x);
+            Console.ReadKey();
+            
+
+        }
+        public static void printConsole(double y)
+        {
+            Console.WriteLine(y);
+        }
+        public static double getInput()
+        {
+            
+            double result = Double.Parse(Console.ReadLine());
+            return result;
         }
     }
 }
